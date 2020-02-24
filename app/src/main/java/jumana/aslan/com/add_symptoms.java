b9817d4953791db8f95bfb2c0cb3f47a0e366060
Ayah <aslanayah3@gmail.com>
2020-02-24 09:37:56 +0200

    first step

--- app/src/main/java/jumana/aslan/com/add_symptoms.java
+++ app/src/main/java/jumana/aslan/com/add_symptoms.java
@@ -3,13 +3,23 @@ package jumana.aslan.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class add_symptoms extends AppCompatActivity {
+    private EditText symTitle,symSub;
+    private Button symSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_symptoms);
+
+        symTitle=findViewById(R.id.symTitle);
+        symSub=findViewById(R.id.symSub);
+        symSave=findViewById(R.id.symSave);
+
     }
 }
 //
--- app/src/main/java/jumana/aslan/com/manger_add.java
+++ app/src/main/java/jumana/aslan/com/manger_add.java
@@ -3,12 +3,23 @@ package jumana.aslan.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class manger_add extends AppCompatActivity {
+    private EditText manTitle,manSub;
+    private Button manSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manger_add);
+
+        manTitle=findViewById(R.id.manTitle);
+        manSub=findViewById(R.id.manSub);
+        manSave=findViewById(R.id.manSave);
     }
 }
+
+
