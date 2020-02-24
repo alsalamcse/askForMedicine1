f33dd89e58d5826dce0c8806d75556c359965f0a
JamalSamniya <jamal.samniya@gmail.com>
2020-02-24 10:49:21 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/jumana/aslan/com/Search.java
+++ app/src/main/java/jumana/aslan/com/Search.java
@@ -3,12 +3,36 @@ package jumana.aslan.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
+    private SearchView searchV;
+    private TextView tvPeo,tvExp;
+    private FloatingActionButton addPeo,addExp;
+    private ListView listV;
+
+    ArrayList<String>list;
+    ArrayAdapter<String>adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        searchV=findViewById(R.id.searchV);
+        tvExp=findViewById(R.id.tvExp);
+        tvPeo=findViewById(R.id.tvPeo);
+        addExp=findViewById(R.id.addExp);
+        addPeo=findViewById(R.id.addPeo);
+        listV=findViewById(R.id.listV);
+
+
     }
 }
